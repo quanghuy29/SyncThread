b7c1286d5fa277e57beb073d054b2fc4845db7b4
quanghuy29 <73057345+quanghuy29@users.noreply.github.com>
2021-12-12 09:36:32 +0700

    Add files via upload

---
+++ src/ThreadedWorkerWithLock.java
@@ -0,0 +1,12 @@
+public class ThreadedWorkerWithLock extends Thread{
+    private ResourcesExploiterWithLock rExp;
+    public ThreadedWorkerWithLock(ResourcesExploiterWithLock resource) {
+        this.rExp = resource;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++)
+            rExp.exploit();
+    }
+}
---
+++ src/mains.java
@@ -0,0 +1,18 @@
+public class mains {
+    public static void main(String[] args) throws InterruptedException {
+        ResourcesExploiterWithLock resource = new ResourcesExploiterWithLock(0);
+
+        ThreadedWorkerWithLock worker1 = new ThreadedWorkerWithLock(resource);
+        ThreadedWorkerWithLock worker2 = new ThreadedWorkerWithLock(resource);
+        ThreadedWorkerWithLock worker3 = new ThreadedWorkerWithLock(resource);
+
+        worker1.start();
+        worker2.start();
+        worker3.start();
+
+        worker1.join();
+        worker2.join();
+        worker3.join();
+        System.out.println(resource.getRsc());
+    }
+}
